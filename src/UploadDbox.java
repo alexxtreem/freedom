466f23cfb4ce41397c5bb0d3465c4ea7f81ce624
Aleksey Tverdovskiy <alexxtreem@mail.ru>
2021-01-16 21:49:30 +0300

    With new token to the old vrrsion

--- src/UploadDbox.java
+++ src/UploadDbox.java
@@ -3,7 +3,7 @@ import com.dropbox.core.v2.DbxClientV2;
 
 public class UploadDbox {
     public static void main(String[] args) throws Exception {
-        String ACCESS_TOKEN = "sl.ApcxPySBwPIXoaye0Of9eK5DkO7g1yVIwQ61cDdWtXZWdMsoBybggLPH0j2mr7ZH8FkHnQBKJPKXhAoa7TfFT9of3mkg0yk6JAbbqodEeCWvwQXqCTQn5mxEzliVvvkSGDT6F1s";
+        String ACCESS_TOKEN = "qnlsCpMtUEQAAAAAAAAAAQG00liBm523krKUSvicMNLTq__50hAuzvJauV7YBHsg";
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
         ScreenFoto thread = new ScreenFoto(client);
