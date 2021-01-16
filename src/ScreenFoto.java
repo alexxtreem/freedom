3ebfd5f6dca6f3aea871661cba9808c55e2dc2f8
Aleksey Tverdovskiy <alexxtreem@mail.ru>
2021-01-16 21:44:12 +0300

    New code is good with new token

--- src/ScreenFoto.java
+++ src/ScreenFoto.java
@@ -41,34 +41,3 @@ public class ScreenFoto extends Thread {
         }
     }
 }
-
-
-
-//import com.dropbox.core.DbxRequestConfig;
-//import com.dropbox.core.v2.DbxClientV2;
-//import com.dropbox.core.v2.files.FileMetadata;
-//import java.io.FileInputStream;
-//import java.io.InputStream;
-//
-//public class ScreenFoto {
-//    public static void main(String[] args) {
-//
-//        String ACCESS_TOKEN = "sl.Ape7fzDbU6pAFpTqdquPJO94yekCkgDy9DTxX-FaOy3JagSYd15Ys9kLhZ59bDWIVIt9Dhd_FOhKJ9MdJ820SaFBj_FoNftCuNglyAPCOsFkrPAyEzPZc129hUauzzrUSjvfc3w";
-//
-//        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
-//        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
-//
-//        try {
-//            InputStream in = new FileInputStream("D:/SkillBox/IntensJ0114/camomile_PNG666.png");
-//            client.files().uploadBuilder("/test.txt").uploadAndFinish(in);
-//        }
-//        catch (Exception ex) {
-//            ex.printStackTrace();
-//        }
-//
-//
-//
-////        UplpadDbox trhread = new UplpadDbox();
-////        trhread.start();
-//    }
-//}
--- src/UploadDbox.java
+++ src/UploadDbox.java
@@ -3,7 +3,7 @@ import com.dropbox.core.v2.DbxClientV2;
 
 public class UploadDbox {
     public static void main(String[] args) throws Exception {
-        String ACCESS_TOKEN = "sl.Ape7fzDbU6pAFpTqdquPJO94yekCkgDy9DTxX-FaOy3JagSYd15Ys9kLhZ59bDWIVIt9Dhd_FOhKJ9MdJ820SaFBj_FoNftCuNglyAPCOsFkrPAyEzPZc129hUauzzrUSjvfc3w";
+        String ACCESS_TOKEN = "sl.ApcxPySBwPIXoaye0Of9eK5DkO7g1yVIwQ61cDdWtXZWdMsoBybggLPH0j2mr7ZH8FkHnQBKJPKXhAoa7TfFT9of3mkg0yk6JAbbqodEeCWvwQXqCTQn5mxEzliVvvkSGDT6F1s";
         DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
         DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
         ScreenFoto thread = new ScreenFoto(client);
@@ -11,33 +11,3 @@ public class UploadDbox {
     }
 }
 
-
-
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//
-//public class UplpadDbox extends Thread {
-////    FileInputStream inputStream;
-////
-//
-//
-//    @Override
-//    public void run()
-//    {
-//
-//        for (int i = 0; i < 100; i++) {
-//            System.out.println(i);
-//
-//            try {
-//                sleep(1000);
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        try {
-//            inputStream = new FileInputStream("D:\\SkillBox\\IntensJ0114\\camomile_PNG666.png");
-//        } catch (FileNotFoundException e) {
-//            e.printStackTrace();
-//        }
-//    }
-//}
